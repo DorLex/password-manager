75c79f3f9b084725aac14b71be1fd6cfa0cb6b44
DorLex <dorozhinskiysanya@gmail.com>
2023-12-05 21:58:27 +0300

    refact views

--- password_manager/views.py
+++ password_manager/views.py
@@ -1,4 +1,6 @@
+from rest_framework import status
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,6 +12,7 @@ from .services import get_service_by_service_name, get_services_ilike_service_na
 
 
 class ServicePasswordAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request, service_name):
         """Получаем пароль по имени сервиса"""
@@ -24,26 +27,23 @@ class ServicePasswordAPIView(APIView):
     def post(self, request, service_name):
         """Создаём пароль"""
 
+        service = get_service_by_service_name(service_name)
+        if service:
+            return Response({'detail': 'service c таким именем уже есть'}, status.HTTP_400_BAD_REQUEST)
+
         # Используем шифрование, а не хеширование, что бы возвращать пароль клиенту в исходном виде.
         # Т. к. шифрование работает в обе стороны, а хеширование - только в одну.
-
         encrypt_data = get_encrypt_data(request.data)
 
-        service = get_service_by_service_name(service_name)
+        encrypt_data['service_name'] = service_name
+        serializer = ServicePasswordSerializer(data=encrypt_data)
 
-        if service:
-            return Response({'mess': 'service уже есть'})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        else:
-            encrypt_data['service_name'] = service_name
-            serializer = ServicePasswordSerializer(data=encrypt_data)
+        decrypt_response_data = get_decrypt_data(serializer.data)
 
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-
-            decrypt_response_data = get_decrypt_data(serializer.data)
-
-            return Response(decrypt_response_data)
+        return Response(decrypt_response_data)
 
     def patch(self, request, service_name):
         """Заменяем существующий пароль"""
@@ -63,6 +63,7 @@ class ServicePasswordAPIView(APIView):
 
 
 class ServicePasswordILikeAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
         """Проводим поиск по части названия службы и выдаём пароли с подходящими service_name"""
