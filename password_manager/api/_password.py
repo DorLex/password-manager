e8d62dc2beb476a9e9afbb56fe7ddf448b053489
DorLex <dorozhinskiysanya@gmail.com>
2025-11-24 19:06:13 +0300

    refact: refact all

---
+++ password_manager/api/_password.py
@@ -0,0 +1,61 @@
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from encryption.decrypt import get_decrypt_data
+from encryption.encrypt import get_encrypt_data
+from password_manager.models import ServicePassword
+from password_manager.serializers import ServicePasswordSerializer
+from password_manager.services import check_service_exists
+
+
+class ServicePasswordAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, _request: Request, service_name: str) -> Response:
+        """Получаем пароль по имени сервиса"""
+
+        service = get_object_or_404(ServicePassword, service_name=service_name)
+        serializer = ServicePasswordSerializer(service)
+
+        decrypt_response_data = get_decrypt_data(serializer.data)
+
+        return Response(decrypt_response_data)
+
+    def post(self, request: Request, service_name: str) -> Response:
+        """Создаём пароль"""
+
+        service_exists = check_service_exists(service_name)
+        if service_exists:
+            return Response({'detail': 'service c таким именем уже есть'}, status.HTTP_400_BAD_REQUEST)
+
+        # Используем шифрование, а не хеширование, что бы возвращать пароль клиенту в исходном виде.
+        # Т. к. шифрование работает в обе стороны, а хеширование - только в одну.
+        encrypt_data = get_encrypt_data(request.data)
+        encrypt_data['service_name'] = service_name
+
+        serializer = ServicePasswordSerializer(data=encrypt_data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        decrypt_response_data = get_decrypt_data(serializer.data)
+
+        return Response(decrypt_response_data)
+
+    def patch(self, request: Request, service_name: str) -> Response:
+        """Заменяем существующий пароль"""
+
+        encrypt_data = get_encrypt_data(request.data)
+
+        service = get_object_or_404(ServicePassword, service_name=service_name)
+
+        serializer = ServicePasswordSerializer(service, encrypt_data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        decrypt_response_data = get_decrypt_data(serializer.data)
+
+        return Response(decrypt_response_data)
--- password_manager/urls.py
+++ password_manager/urls.py
@@ -1,9 +1,28 @@
 from django.urls import URLPattern, path
 
-from .api.password import ServicePasswordAPIView
-from .api.search import ServicePasswordILikeAPIView
+from .api.password import ServicePasswordViewSet
 
 urlpatterns: list[URLPattern] = [
-    path('password/<slug:service_name>', ServicePasswordAPIView.as_view(), name='password_by_service_name'),
-    path('password/', ServicePasswordILikeAPIView.as_view(), name='password_ilike_service_name'),
+    # path('password/<slug:service_name>', ServicePasswordAPIView.as_view(), name='password_by_service_name'),
+    # path('password/', ServicePasswordILikeAPIView.as_view(), name='password_ilike_service_name'),
+    path(
+        'passwords/',
+        ServicePasswordViewSet.as_view(
+            {
+                'get': 'list',
+                'post': 'create',
+            },
+        ),
+        name='passwords-list',
+    ),
+    path(
+        'passwords/<slug:service_name>/',
+        ServicePasswordViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'update',
+            },
+        ),
+        name='passwords-detail',
+    ),
 ]
